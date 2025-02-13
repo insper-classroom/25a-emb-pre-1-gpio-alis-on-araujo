ba73188dc1e49f2626f7116af20bb8daa6f43c7b
Alison Araujo <alisonao@al.insper.edu.br>
2025-02-13 14:04:59 -0300

    e5

--- exe5/main.c
+++ exe5/main.c
@@ -15,20 +15,20 @@ int main() {
     gpio_init(BTN_PIN_2);
     gpio_set_dir(BTN_PIN_2, GPIO_IN);
     gpio_pull_up(BTN_PIN_2);
-    int cnt_1 = 0;
-    int cnt_2 = 0;
+    int cnt_1 = 1;
+    int cnt_2 = 1;
 
     while (true) {
         if (!gpio_get(BTN_PIN)) {
-            cnt_1++;
             printf("Botão 1: %d \n", cnt_1);
             sleep_ms(300);
+            cnt_1++;
         }
 
         if (!gpio_get(BTN_PIN_2)) {
-            cnt_2++;
             printf("Botão 2: %d \n", cnt_2);
             sleep_ms(300);
+            cnt_2++;
         }
     }
 }
