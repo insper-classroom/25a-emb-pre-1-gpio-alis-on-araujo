045bf282fe1bcbbd262a07779f227a3f78ef076a
Alison Araujo <alisonao@al.insper.edu.br>
2025-02-13 17:31:32 -0300

    5 de novo

--- exe5/main.c
+++ exe5/main.c
@@ -21,13 +21,13 @@ int main() {
     while (true) {
         if (!gpio_get(BTN_PIN)) {
             printf("Botao 1: %d \n", cnt_1);
-            sleep_ms(300);
+            sleep_ms(500);
             cnt_1++;
         }
 
         if (!gpio_get(BTN_PIN_2)) {
             printf("Botao 2: %d \n", cnt_2);
-            sleep_ms(300);
+            sleep_ms(500);
             cnt_2++;
         }
     }
